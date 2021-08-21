c4f7b24c1cb7b60ed1120a2cf9250232b745a692
Stjin <stjin.stijnvandewater@gmail.com>
2021-08-21 14:37:42 +0200

    - Fix integration not working on some devices - Improved integration speed

--- app/src/main/java/host/stjin/anonaddy/NetworkHelper.kt
+++ app/src/main/java/host/stjin/anonaddy/NetworkHelper.kt
@@ -3,6 +3,7 @@ package host.stjin.anonaddy
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import com.einmalfel.earl.EarlParser
 import com.einmalfel.earl.Feed
@@ -323,6 +324,7 @@ class NetworkHelper(private val context: Context) {
         if (BuildConfig.DEBUG) {
             println("${object {}.javaClass.enclosingMethod?.name} called from ${Thread.currentThread().stackTrace[3].className};${Thread.currentThread().stackTrace[3].methodName}")
         }
+        Log.e("KEK", "1")
 
 
         /*
@@ -341,6 +343,7 @@ class NetworkHelper(private val context: Context) {
         if (!page.isNullOrEmpty()) {
             parameters.add("page[number]" to page)
         }
+        Log.e("KEK", "2")
 
         val (_, response, result) = Fuel.get(API_URL_ALIAS, parameters)
             .appendHeader(
@@ -350,7 +353,7 @@ class NetworkHelper(private val context: Context) {
                 "Accept" to "application/json"
             )
             .awaitStringResponseResult()
-
+        Log.e("KEK", "3")
 
 
         when (response.statusCode) {
