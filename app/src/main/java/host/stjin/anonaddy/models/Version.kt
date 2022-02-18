25286ff6c269493c3c5510fdf2ef3bb5c822aa9d
Stjin <stjin.stijnvandewater@gmail.com>
2022-02-18 18:54:47 +0100

    - Fixed a bug where self-hosted instances with version 0.10.0 or higher would be marked as incompatible

--- app/src/main/java/host/stjin/anonaddy/models/Version.kt
+++ app/src/main/java/host/stjin/anonaddy/models/Version.kt
@@ -1,8 +1,8 @@
 package host.stjin.anonaddy.models
 
 data class Version(
-    val major: Int?,
-    val minor: Int?,
-    val patch: Int?,
+    val major: Int,
+    val minor: Int,
+    val patch: Int,
     val version: String?
 )
