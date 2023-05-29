45a4939cfd2ab099d52ff6055349d213bc65093b
Stjin <stjin.stijnvandewater@gmail.com>
2023-05-29 10:56:16 +0200

    - Updated libraries - Fixed a bug in UI and crash for certain devices

--- app/src/main/java/host/stjin/anonaddy/utils/WebIntentManager.kt
+++ app/src/main/java/host/stjin/anonaddy/utils/WebIntentManager.kt
@@ -17,32 +17,30 @@ import host.stjin.anonaddy_shared.AnonAddy
 
 class WebIntentManager(private val context: Context) {
     fun requestSupportedLinks(enable: Boolean) {
-        when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val intent = Intent(
-                    Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS,
-                    Uri.parse("package:${context.packageName}")
-                )
-                context.startActivity(intent)
-                if (enable) {
-                    Toast.makeText(context, context.resources.getString(R.string.webintent_manage_default_apps_enable), Toast.LENGTH_LONG).show()
-                } else {
-                    Toast.makeText(context, context.resources.getString(R.string.webintent_manage_default_apps_disable), Toast.LENGTH_LONG).show()
-                }
+        try {
+            val intent = Intent(
+                Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS,
+                Uri.parse("package:${context.packageName}")
+            )
+            context.startActivity(intent)
+            if (enable) {
+                Toast.makeText(context, context.resources.getString(R.string.webintent_manage_default_apps_enable), Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(context, context.resources.getString(R.string.webintent_manage_default_apps_disable), Toast.LENGTH_LONG).show()
             }
-            else -> {
-                val intent = Intent()
-                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                val uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
-                intent.data = uri
-                context.startActivity(intent)
-                if (enable) {
-                    Toast.makeText(context, context.resources.getString(R.string.webintent_manage_default_apps_pre_sdk24_enable), Toast.LENGTH_LONG)
-                        .show()
-                } else {
-                    Toast.makeText(context, context.resources.getString(R.string.webintent_manage_default_apps_pre_sdk24_disable), Toast.LENGTH_LONG)
-                        .show()
-                }
+
+        } catch (_: Exception) {
+            val intent = Intent()
+            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            val uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+            intent.data = uri
+            context.startActivity(intent)
+            if (enable) {
+                Toast.makeText(context, context.resources.getString(R.string.webintent_manage_default_apps_pre_sdk24_enable), Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                Toast.makeText(context, context.resources.getString(R.string.webintent_manage_default_apps_pre_sdk24_disable), Toast.LENGTH_LONG)
+                    .show()
             }
         }
     }
