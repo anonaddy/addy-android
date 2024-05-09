042bf5a95e224a9f801ebd530c8c9bd6b5750250
Stjin <stjin.stijnvandewater@gmail.com>
2024-05-09 13:50:09 +0200

    - Make nullable

--- anonaddy_shared/src/main/java/host/stjin/anonaddy_shared/models/Aliases.kt
+++ anonaddy_shared/src/main/java/host/stjin/anonaddy_shared/models/Aliases.kt
@@ -42,12 +42,12 @@ data class Aliases(
 
 data class Meta(
     val current_page: Int,
-    val from: Int,
+    val from: Int?,
     val last_page: Int,
     val links: List<Link>,
     val path: String,
     val per_page: Int,
-    val to: Int,
+    val to: Int?,
     val total: Int
 )
 
