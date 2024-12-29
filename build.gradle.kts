758b296d402d9fc93f013cb7a91dbd71463c1fdf
Stjin <stjin.stijnvandewater@gmail.com>
2024-12-29 13:10:46 +0100

    - Updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.2")
+        classpath("com.android.tools.build:gradle:8.7.3")
         classpath(kotlin("gradle-plugin", version = "2.0.0"))
 
         // NOTE: Do not place your application dependencies here; they belong
