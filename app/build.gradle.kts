83c1a7d77af12a1bba1edd8600128db039998dbf
Stjin <stjin.stijnvandewater@gmail.com>
2026-01-10 12:52:50 +0100

    Fix https://github.com/anonaddy/addy-android/issues/49

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,9 +19,9 @@ android {
          */
 
         // SDK 36 + v5.9.0 + release 01 + 00 (for app)
-        versionCode = 365900200 // https://developer.android.com/training/wearables/packaging
+        versionCode = 365900100 // https://developer.android.com/training/wearables/packaging
         // The "v" is important, as the updater class compares with the RSS feed on Github
-        versionName = "v5.9.0"
+        versionName = "v5.9.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
